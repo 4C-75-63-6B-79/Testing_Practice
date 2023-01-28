3fcac70e12cc234bf8bcda50cb92cab780cb916a
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-28 13:01:20 +0530

    modified the function reverseString to pass all the test

--- src/reverseStringFunction.js
+++ src/reverseStringFunction.js
@@ -1,3 +1,10 @@
-export default reverseString() {
-    return ;
+export default function reverseString(word) {
+    let reverseWord = '';
+    if(!word) {
+        return 'true';
+    }
+    for(let i=0; i<word.length; i+=1) {
+        reverseWord = word.charAt(i) + reverseWord;
+    }
+    return reverseWord;
 }
