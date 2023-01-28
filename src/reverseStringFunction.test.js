63f85f679260ebc73c7ee137a433f76a78cd7016
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-28 12:46:38 +0530

    added test for reverseString function

--- src/reverseStringFunction.test.js
+++ src/reverseStringFunction.test.js
@@ -4,4 +4,16 @@ import reverseString from './reverseStringFunction';
 
 test('reverseString returns', () => {
     expect(reverseString()).toHaveReturned();
-})
+});
+
+test('reverseString returns string', () => {
+    expect(typeof reverseString()).toBe('string');
+});
+
+test('reverseString returns string of same length', () => {
+    expect(reverseString('word').length).toBe('word'.length);
+});
+
+test('reverseString returns string with it character in reverse order', () => {
+    expect(reverseString('word')).toBe('drow');
+});
