831b441463c61435b1d61003e3237d61083ab9f3
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-28 16:46:19 +0530

    modified the add and subtract function to pass the test

--- src/calculatorFunction.js
+++ src/calculatorFunction.js
@@ -1,11 +1,26 @@
-function getCalculator() {
-    function add() {
-        return 0;
-    }
+function Calculator() {
 
-    return {
-        add,
-    };
-};
+}
 
-export default getCalculator;
+Calculator.prototype.add = function add(x=0, y=0) {
+    return Number.isInteger(x) && Number.isInteger(y) ? x + y : 'add takes atmost 2 integer arguments';
+}
+
+Calculator.prototype.subtract = function subtract(x=0, y=0) {
+    return Number.isInteger(x) && Number.isInteger(y) ? x - y : 'subtract takes atmost 2 integer arguments';
+}
+
+Calculator.prototype.multiply = function multiply() {
+    return 0;
+}
+
+Calculator.prototype.divide = function divide(x=0, y=1) {
+    return 0;
+}
+
+export default function getCalculator() {
+    const obj = new Calculator();
+    return obj;
+}
+
+export {Calculator};
