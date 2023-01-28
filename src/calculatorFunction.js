6062a7da5c8c58c3da3be4c2175022325546482d
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-28 17:07:58 +0530

    created the function divide to pass the test

--- src/calculatorFunction.js
+++ src/calculatorFunction.js
@@ -10,12 +10,12 @@ Calculator.prototype.subtract = function subtract(x=0, y=0) {
     return Number.isInteger(x) && Number.isInteger(y) ? x - y : 'subtract takes atmost 2 integer arguments';
 }
 
-Calculator.prototype.multiply = function multiply() {
-    return 0;
+Calculator.prototype.multiply = function multiply(x=0, y=0) {
+    return Number.isInteger(x) && Number.isInteger(y) ? x * y : 'multiply takes atmost 2 integer arguments';
 }
 
-Calculator.prototype.divide = function divide(x=0, y=1) {
-    return 0;
+Calculator.prototype.divide = function divide(x=0, y=0) {
+    return Number.isInteger(x) && Number.isInteger(y) ? x / y : 'divide takes atmost 2 integer arguments';
 }
 
 export default function getCalculator() {
